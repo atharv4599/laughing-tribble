66975dd4f77cdbcc081ea8360050ef922f1cf15c
atharv4599 <athu4599@gmail.com>
2023-03-17 12:20:53 +0530

    AAAA

--- A.java
+++ A.java
@@ -1,4 +1,4 @@
-Public class A{
+public class A{
     public void add()
     {
         int a=10;
@@ -6,9 +6,16 @@ Public class A{
         int c=a+b;
         System.out.println(c);
     }
+    public void sub()
+    {
+        int a =70;
+        int b = 100;
+        System.out.println(a+b);
+    }
     public static void main(String args[])
     {
         A a=new A();
         a.add();
+        a.sub();
     }
 }
