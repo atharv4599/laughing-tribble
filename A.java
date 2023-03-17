04e23eb5981f37165ff987f2568cf3bb708fbc73
Atlee29 <pkbarada@outlook.com>
2023-03-17 12:22:54 +0530

    Atlee

--- A.java
+++ A.java
@@ -1,4 +1,4 @@
-Public class A{
+public class A{
     public void add()
     {
         int a=10;
@@ -6,9 +6,16 @@ Public class A{
         int c=a+b;
         System.out.println(c);
     }
+    public void mul(){
+        int a1=5;
+        int a2=10;
+        int c1=a1*b1;
+        System.out.println(c1);
+    }
     public static void main(String args[])
     {
         A a=new A();
         a.add();
+        a.mul();
     }
 }
